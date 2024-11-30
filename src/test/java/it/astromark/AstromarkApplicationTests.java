67932da24c83fbe139986ce7304fa941cc56c911
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2024-11-30 21:15:28 +0100

    feat(core): add spring project base structure

---
+++ src/test/java/it/astromark/AstromarkApplicationTests.java
@@ -0,0 +1,13 @@
+package it.astromark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstromarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
