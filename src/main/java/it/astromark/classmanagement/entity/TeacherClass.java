e38f0c35955b72cc0a692992471ea880abdea68b
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2024-12-22 20:04:50 +0100

    feat(jpa): add SchoolClass and associative class TeacherClass

---
+++ src/main/java/it/astromark/classmanagement/entity/TeacherClass.java
@@ -0,0 +1,42 @@
+package it.astromark.classmanagement.entity;
+
+import it.astromark.user.teacher.entity.Teacher;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "teacher_class", schema = "astromark")
+public class TeacherClass {
+    @EmbeddedId
+    private TeacherClassId id;
+
+    @MapsId("teacherId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.RESTRICT)
+    @JoinColumn(name = "teacher_id", nullable = false)
+    private Teacher teacher;
+
+    @MapsId("schoolClassId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.RESTRICT)
+    @ColumnDefault("nextval('astromark.teacher_class_school_class_id_seq')")
+    @JoinColumn(name = "school_class_id", nullable = false)
+    private SchoolClass schoolClass;
+
+    @NotNull
+    @ColumnDefault("false")
+    @Column(name = "coordinator", nullable = false)
+    private Boolean coordinator = false;
+
+}
