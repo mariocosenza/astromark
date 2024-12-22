8150d5dee2c8c8b4d1d5dc37a4a71c65ce553dc8
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2024-12-22 20:02:14 +0100

    feat(jpa): add entity School

---
+++ src/main/java/it/astromark/school/entity/School.java
@@ -0,0 +1,74 @@
+package it.astromark.school.entity;
+
+import it.astromark.classmanagement.entity.SchoolClass;
+import it.astromark.user.parent.entity.Parent;
+import it.astromark.user.secretary.entity.Secretary;
+import it.astromark.user.student.entity.Student;
+import it.astromark.user.teacher.entity.Teacher;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "school", schema = "astromark")
+public class School {
+    @Id
+    @Size(max = 7)
+    @SequenceGenerator(name = "school_id_gen", sequenceName = "school_class_id_seq", allocationSize = 1)
+    @Column(name = "code", nullable = false, length = 7)
+    private String code;
+
+    @NotNull
+    @Column(name = "phone_number", nullable = false)
+    private Integer phoneNumber;
+
+    @Size(max = 512)
+    @NotNull
+    @Column(name = "address", nullable = false, length = 512)
+    private String address;
+
+    @Size(max = 256)
+    @Column(name = "name", length = 256)
+    private String name;
+
+    @Size(max = 256)
+    @NotNull
+    @Column(name = "email", nullable = false, length = 256)
+    private String email;
+
+    @Size(max = 129)
+    @Column(name = "school_principal_full_name", length = 129)
+    private String schoolPrincipalFullName;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "schoolCode")
+    private Set<Parent> parents = new LinkedHashSet<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "schoolCode")
+    private Set<SchoolClass> schoolClasses = new LinkedHashSet<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "schoolCode")
+    private Set<Secretary> secretaries = new LinkedHashSet<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "schoolCode")
+    private Set<Student> students = new LinkedHashSet<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "schoolCode")
+    private Set<Teacher> teachers = new LinkedHashSet<>();
+
+}
