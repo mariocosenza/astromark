5635eea0901d66126e23c316ea0c174f072dcec6
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2024-12-23 15:58:14 +0100

    feat(api): configure frontend and rest api endpoints

---
+++ src/main/java/it/astromark/commons/SpringConfig.java
@@ -0,0 +1,75 @@
+package it.astromark.commons;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
+
+import java.util.List;
+
+import static java.util.Objects.nonNull;
+
+@Configuration
+@EnableWebSecurity
+public class SpringConfig implements WebMvcConfigurer {
+
+
+    /**
+     * Temporary bean to disable default http basic auth
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()).build();
+    }
+
+    /**
+     * Disable CORS for frontend page served in static folder
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("*");
+    }
+
+    /**
+     * Add resource handler to serve web pages in static folder
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        this.serveDirectory(registry, "/", "classpath:/static/");
+    }
+
+    private void serveDirectory(ResourceHandlerRegistry registry, String endpoint, String location) {
+        String[] endpointPatterns = endpoint.endsWith("/")
+                ? new String[]{endpoint.substring(0, endpoint.length() - 1), endpoint, endpoint + "**"}
+                : new String[]{endpoint, endpoint + "/", endpoint + "/**"};
+        registry
+                .addResourceHandler(endpointPatterns)
+                .addResourceLocations(location.endsWith("/") ? location : location + "/")
+                .resourceChain(false)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    public Resource resolveResource(HttpServletRequest request, String requestPath, List<? extends Resource> locations, ResourceResolverChain chain) {
+                        Resource resource = super.resolveResource(request, requestPath, locations, chain);
+                        if (nonNull(resource)) {
+                            return resource;
+                        }
+                        return super.resolveResource(request, "/index.html", locations, chain);
+                    }
+                });
+    }
+
+}
