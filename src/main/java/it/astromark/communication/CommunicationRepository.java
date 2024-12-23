e67dbd08ac350333cd2b912e04917588c9243e20
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2024-12-23 21:19:34 +0100

    feat(backend): add JpaRepository related to school class activity

---
+++ src/main/java/it/astromark/communication/CommunicationRepository.java
@@ -0,0 +1,9 @@
+package it.astromark.communication;
+
+import it.astromark.communication.entity.Communication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommunicationRepository extends JpaRepository<Communication, Integer> {
+}
