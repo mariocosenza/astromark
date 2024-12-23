51f4779d48c2672a035e7e3b052badf040ec59c0
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2024-12-23 21:18:13 +0100

    feat(backend): add JpaRepository for teacher related entities

---
+++ src/main/java/it/astromark/user/teacher/repository/TeacherRepository.java
@@ -0,0 +1,11 @@
+package it.astromark.user.teacher.repository;
+
+import it.astromark.user.teacher.entity.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
+}
