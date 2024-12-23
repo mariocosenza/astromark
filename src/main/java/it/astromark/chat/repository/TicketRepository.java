d3349b98c194aabc79da90080d3f14eab4da5b96
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2024-12-23 21:20:06 +0100

    feat(backend): add JpaRepository related to chat

---
+++ src/main/java/it/astromark/chat/repository/TicketRepository.java
@@ -0,0 +1,11 @@
+package it.astromark.chat.repository;
+
+import it.astromark.chat.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+}
