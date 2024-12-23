900d53bc8bc638b96d1d8884457d458e20fb92cf
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2024-12-23 21:15:38 +0100

    feat(jpa): add jakarta validation for ReceptionTimeslot

--- src/main/java/it/astromark/agenda/reception/entity/ReceptionTimeslot.java
+++ src/main/java/it/astromark/agenda/reception/entity/ReceptionTimeslot.java
@@ -2,7 +2,9 @@ package it.astromark.agenda.reception.entity;
 
 import it.astromark.agenda.commons.entity.Timeslot;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -21,7 +23,11 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-@Table(name = "reception_timeslot", schema = "astromark")
+@Table(
+        name = "reception_timeslot",
+        schema = "astromark",
+        uniqueConstraints = {@UniqueConstraint(name = "uk_reception_timeslot", columnNames = {"reception_timetable_id", "hour", "date"})}
+)
 public class ReceptionTimeslot extends Timeslot {
 
 
@@ -34,16 +40,19 @@ public class ReceptionTimeslot extends Timeslot {
 
     @NotNull
     @ColumnDefault("6")
+    @PositiveOrZero
     @Column(name = "capacity", nullable = false)
     private Short capacity;
 
     @NotNull
     @ColumnDefault("0")
+    @PositiveOrZero
     @Column(name = "booked", nullable = false)
     private Short booked;
 
     @Size(max = 128)
     @NotNull
+    @NotBlank
     @Column(name = "mode", nullable = false, length = 128)
     private String mode;
 
