f368226bb6999cb1d818b8d23e751a3d5cb73e50
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2024-12-28 22:51:19 +0100

    feat(frontend): add route for login

--- src/main/typescript/src/main.tsx
+++ src/main/typescript/src/main.tsx
@@ -1,16 +1,20 @@
 import ReactDOM from "react-dom/client";
-import {BrowserRouter} from "react-router";
-import './index.css'
+import {BrowserRouter, Route, Routes} from "react-router";
+import './styles/index.css'
 import {ThemeProvider} from "@mui/material";
-import {theme} from "./theme/GlobalTheme.ts";
-import {App} from "./App.tsx";
+import {theme} from "./theme/GlobalTheme";
+import {App} from "./pages/App";
+import {Login} from "./pages/Login.tsx";
 
 const root: HTMLElement = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(root).render(
     <ThemeProvider theme={theme}>
         <BrowserRouter>
-                <App/>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
         </BrowserRouter>
     </ThemeProvider>
 )
