997b6570ee7005ee5214168a2a9e3c2d23160f41
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2024-12-29 20:39:46 +0100

    fix(frontend): add StrictMode for better debugging

--- src/main/typescript/src/main.tsx
+++ src/main/typescript/src/main.tsx
@@ -5,16 +5,19 @@ import {ThemeProvider} from "@mui/material";
 import {theme} from "./theme/GlobalTheme";
 import {App} from "./pages/App";
 import {Login} from "./pages/Login.tsx";
+import {StrictMode} from "react";
 
 const root: HTMLElement = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(root).render(
-    <ThemeProvider theme={theme}>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/login" element={<Login/>}/>
-            </Routes>
-        </BrowserRouter>
-    </ThemeProvider>
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="/login" element={<Login/>}/>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    </StrictMode>
 )
