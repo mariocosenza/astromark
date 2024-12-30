0865bee4fbb855282c72cd09407ba832e74db313
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2024-12-30 20:08:03 +0100

    fix(security): update path to /api base

--- src/main/java/it/astromark/authentication/controller/AuthController.java
+++ src/main/java/it/astromark/authentication/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/")
 public class AuthController {
 
     private final AuthenticationService authenticationService;
