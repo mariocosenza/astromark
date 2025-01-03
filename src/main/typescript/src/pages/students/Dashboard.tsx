c7ff951b6bddc7906fcdf751765234f6b9dffb27
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-01-03 20:17:56 +0100

    feat(frontend): add placeholder pages

---
+++ src/main/typescript/src/pages/students/Dashboard.tsx
@@ -0,0 +1,9 @@
+import {StudentParentSideNav} from "../../components/StudentParentSideNav.tsx";
+
+export const Dashboard: React.FC = () => {
+    return (
+        <div>
+            <StudentParentSideNav/>
+        </div>
+    );
+}
