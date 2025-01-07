d2af4661b6650f2300e91e81a2cf42cd88cc6432
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-01-07 20:44:35 +0100

    feat(backend): add StudentService

--- src/main/java/it/astromark/user/commons/service/SchoolUserService.java
+++ src/main/java/it/astromark/user/commons/service/SchoolUserService.java
@@ -6,4 +6,5 @@ import java.util.UUID;
 
 public interface SchoolUserService {
     boolean isStudentParent(Parent parent, UUID studentId);
+    boolean isLoggedUserParent(UUID studentId);
 }
--- src/main/java/it/astromark/user/commons/service/SchoolUserServiceImpl.java
+++ src/main/java/it/astromark/user/commons/service/SchoolUserServiceImpl.java
@@ -1,6 +1,8 @@
 package it.astromark.user.commons.service;
 
+import it.astromark.authentication.service.AuthenticationService;
 import it.astromark.user.parent.entity.Parent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -8,9 +10,23 @@ import java.util.UUID;
 @Service
 public class SchoolUserServiceImpl implements SchoolUserService {
 
+    private final AuthenticationService authenticationService;
+
+    @Autowired
+    public SchoolUserServiceImpl(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 
     @Override
     public boolean isStudentParent(Parent parent, UUID studentId) {
-        return parent.getStudents().stream().anyMatch(s -> s.getId().equals(studentId));
+        return parent.getStudents().stream().noneMatch(s -> s.getId().equals(studentId));
+    }
+
+    @Override
+    public boolean isLoggedUserParent(UUID studentId) {
+        if(authenticationService.isParent() && isStudentParent(authenticationService.getParent().orElseThrow(), studentId)) {
+            return false;
+        }
+        return true;
     }
 }
