af5150f0f0db6ef96997c655996a260117815a90
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-01-08 20:25:32 +0100

    fix(frontend): correct state management to prevent loop

--- src/main/typescript/src/services/StateService.ts
+++ src/main/typescript/src/services/StateService.ts
@@ -1,5 +1,17 @@
+import {getId, getRole} from "./AuthService.ts";
+import {Role} from "../components/route/ProtectedRoute.tsx";
+
+
 export class SelectedYear {
-    static year = 2025
+    static get year(): number {
+        return this._year;
+    }
+
+    static set year(value: number) {
+        this._year = value;
+    }
+    private static _year = new Date().getMonth() > 9 ? new Date().getFullYear() : new Date().getFullYear() - 1;
+
 }
 
 export class SelectedClass {
@@ -7,5 +19,16 @@ export class SelectedClass {
 }
 
 export class SelectedStudent {
+    static get id(): string | null {
+        if(getRole() !== Role.STUDENT) {
+            return this._id;
+        } else {
+            return getId()
+        }
+    }
 
+    static set id(value: string) {
+        this._id = value;
+    }
+    private static _id: string | null = null;
 }
