75791e6101dd92a9ea66a82deea9c546c8046979
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-01-08 20:19:47 +0100

    feat(backend): add SecretaryController and TeacherController

---
+++ src/main/java/it/astromark/user/teacher/controller/TeacherController.java
@@ -0,0 +1,11 @@
+package it.astromark.user.teacher.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/teachers")
+public class TeacherController {
+
+
+}
