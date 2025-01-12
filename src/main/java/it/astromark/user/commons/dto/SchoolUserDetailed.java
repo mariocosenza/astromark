d071cfda71a1ef4660837d96eb3ba5c52a7bbd3e
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-01-12 19:22:01 +0100

    feat(backend): add mapper and update SchoolUserDetailed

--- src/main/java/it/astromark/user/commons/dto/SchoolUserDetailed.java
+++ src/main/java/it/astromark/user/commons/dto/SchoolUserDetailed.java
@@ -2,5 +2,5 @@ package it.astromark.user.commons.dto;
 
 import java.time.LocalDate;
 
-public record SchoolUserDetailed(String name, String surname, String email, String residentialAddress, String taxId, LocalDate birthDate) {
+public record SchoolUserDetailed(String name, String surname, String email, String residentialAddress, String taxId, LocalDate birthDate, Boolean gender) {
 }
