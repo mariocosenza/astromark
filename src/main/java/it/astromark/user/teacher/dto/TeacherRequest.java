ebb201ec40150e62e73ee32581f89772e0b51d6f
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-01-18 21:03:24 +0100

    refactor(backend): change gender variable name

--- src/main/java/it/astromark/user/teacher/dto/TeacherRequest.java
+++ src/main/java/it/astromark/user/teacher/dto/TeacherRequest.java
@@ -4,5 +4,11 @@ import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
-public record TeacherRequest(@Size(max = 256) @Email String email, @Size(max = 64) @NotEmpty String name, @Size(max = 64) @NotEmpty String surname, String taxId, @Past LocalDate birthDate, @NotNull Boolean gender, @Size(min = 5) @Pattern(regexp = "^[a-zA-Z0-9\\s.]+$") String residentialAddress) {
+public record TeacherRequest(@Size(max = 256) @Email String email,
+                             @Size(max = 64) @NotEmpty String name,
+                             @Size(max = 64) @NotEmpty String surname,
+                             String taxId, @Past LocalDate birthDate,
+                             @NotNull Boolean male,
+                             @Size(min = 5) @Pattern(regexp = "^[a-zA-Z0-9\\s.]+$")
+                             String residentialAddress) {
 }
