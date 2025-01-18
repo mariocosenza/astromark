7743a34dbd9f78dc6d9081ffe61d3bbda4ff9151
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-01-18 21:05:43 +0100

    feat(backend): add ClassManagement functionality

--- src/main/java/it/astromark/classmanagement/controller/ClassManagementController.java
+++ src/main/java/it/astromark/classmanagement/controller/ClassManagementController.java
@@ -1,17 +1,32 @@
 package it.astromark.classmanagement.controller;
 
+import it.astromark.classmanagement.dto.SchoolClassResponse;
+import it.astromark.classmanagement.service.ClassManagementService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Year;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/class-management")
 public class ClassManagementController {
 
-    @GetMapping("/year")
-    public String getYear() {
-        return "2024";
+    private final ClassManagementService classManagementService;
+
+    public ClassManagementController(ClassManagementService classManagementService) {
+        this.classManagementService = classManagementService;
     }
 
+    @GetMapping("/year")
+    public Year getYear() {
+        return classManagementService.getYear();
+    }
+
+    @GetMapping("/all")
+    List<SchoolClassResponse> getClasses() {
+        return classManagementService.getClasses();
+    }
 
 }
--- src/main/java/it/astromark/classmanagement/mapper/ClassManagementMapper.java
+++ src/main/java/it/astromark/classmanagement/mapper/ClassManagementMapper.java
@@ -9,6 +9,7 @@ import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
+import java.util.Set;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ClassManagementMapper {
@@ -16,6 +17,7 @@ public interface ClassManagementMapper {
     SchoolClassResponse toSchoolClassResponse(SchoolClass schoolClass);
 
     List<SchoolClassResponse> toSchoolClassResponseList(List<SchoolClass> marks);
+    List<SchoolClassResponse> toSchoolClassResponseList(Set<SchoolClass> schoolClasses);
 
     @Mapping(target = "description", source = "studyPlan.title")
     TeacherClassResponse toTeacherClassResponse(SchoolClass schoolClass);
