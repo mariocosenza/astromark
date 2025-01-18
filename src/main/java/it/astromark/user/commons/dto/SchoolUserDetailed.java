692b0622b2454f3dced3999c57654146d2bbaf69
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-01-18 21:02:47 +0100

    fix(backend): add missing uuid

--- src/main/java/it/astromark/user/commons/dto/SchoolUserDetailed.java
+++ src/main/java/it/astromark/user/commons/dto/SchoolUserDetailed.java
@@ -1,6 +1,7 @@
 package it.astromark.user.commons.dto;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
-public record SchoolUserDetailed(String name, String surname, String email, String residentialAddress, String taxId, LocalDate birthDate, Boolean gender) {
+public record SchoolUserDetailed(UUID id, String name, String surname, String email, String residentialAddress, String taxId, LocalDate birthDate, Boolean male) {
 }
