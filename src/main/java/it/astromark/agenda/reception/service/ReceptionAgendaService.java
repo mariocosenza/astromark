9b2f976e8ece28f1bdf9e7f9c4337e4de89bd14b
gsacrestano <g.sacrestano@studenti.unisa.it>
2025-01-22 22:18:44 +0100

    ReceptionAgendaService java doc

--- src/main/java/it/astromark/agenda/reception/service/ReceptionAgendaService.java
+++ src/main/java/it/astromark/agenda/reception/service/ReceptionAgendaService.java
@@ -9,19 +9,106 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ReceptionAgendaService {
+    /**
+     * Books a reception timeslot for a given ID.
+     *
+     * @param receptionTimeslotID the ID of the reception timeslot to be booked
+     * @return true if the timeslot is successfully booked, false otherwise
+     * Pre-condition:
+     * - The `receptionTimeslotID` must not be null.
+     * - The timeslot associated with the given ID must exist and be available for booking.
+     * Post-condition:
+     * - The timeslot is marked as booked.
+     * - Returns true if booking is successful, false if the timeslot is not available or does not exist.
+     */
     boolean book(Integer receptionTimeslotID);
 
+    /**
+     * Confirms a previously booked reception timeslot.
+     *
+     * @param receptionTimeslotID the ID of the reception timeslot to confirm
+     * @return true if the timeslot is successfully confirmed, false otherwise
+     * Pre-condition:
+     * - The `receptionTimeslotID` must not be null.
+     * - The timeslot associated with the given ID must exist and have been booked.
+     * Post-condition:
+     * - The timeslot is marked as confirmed.
+     * - Returns true if confirmation is successful, false otherwise.
+     */
     boolean confirm(Integer receptionTimeslotID);
 
+    /**
+     * Refuses a reception timeslot for a given ID.
+     *
+     * @param receptionTimeslotID the ID of the reception timeslot to refuse
+     * @return true if the timeslot is successfully refused, false otherwise
+     * Pre-condition:
+     * - The `receptionTimeslotID` must not be null.
+     * - The timeslot associated with the given ID must exist and have been booked.
+     * Post-condition:
+     * - The timeslot is marked as refused.
+     * - Returns true if refusal is successful, false otherwise.
+     */
     boolean refuse(Integer receptionTimeslotID);
 
+    /**
+     * Adds a new reception timeslot.
+     *
+     * @param id the ID of the teacher or entity associated with the timeslot
+     * @param request the request containing timeslot details
+     * @return the added `ReceptionTimeslotResponse` object
+     * Pre-condition:
+     * - The `id` must not be null.
+     * - The `request` must not be null and must contain valid timeslot details.
+     * Post-condition:
+     * - A new timeslot is added and returned as a response object.
+     */
     ReceptionTimeslotResponse addTimeslot(Integer id, ReceptionTimeslotRequest request);
 
+    /**
+     * Retrieves a list of not confirmed bookings for a given table.
+     * @param tableId the ID of the table to retrieve bookings for
+     * @return a list of `ReceptionBookingResponse` objects representing not confirmed bookings
+     * Pre-condition:
+     * - The `tableId` must not be null.
+     * Post-condition:
+     * - Returns a list of bookings that are in a "not confirmed" state for the specified table.
+     */
     List<ReceptionBookingResponse> getNotConfirmed(Integer tableId);
 
+    /**
+     * Retrieves a list of refused bookings for a given table.
+     *
+     * @param tableId the ID of the table to retrieve bookings for
+     * @return a list of `ReceptionBookingResponse` objects representing refused bookings
+     * Pre-condition:
+     * - The `tableId` must not be null.
+     * Post-condition:
+     * - Returns a list of bookings that are in a "refused" state for the specified table.
+     */
     List<ReceptionBookingResponse> getRefused(Integer tableId);
 
+    /**
+     * Retrieves a list of all booked reception slots.
+     *
+     * @return a list of `ReceptionBookingResponse` objects representing booked slots
+     * Pre-condition:
+     * - None.
+     * Post-condition:
+     * - Returns a list of all timeslots that are currently booked.
+     */
     List<ReceptionBookingResponse> getBookedSlots();
 
+    /**
+     * Retrieves a list of all reception timeslots for a specific teacher.
+     *
+     * @param teacherID the UUID of the teacher
+     * @return a list of `ReceptionTimeslotResponse` objects representing the teacher's timeslots
+     * Pre-condition:
+     * - The `teacherID` must not be null.
+     * Post-condition:
+     * - Returns a list of timeslots associated with the specified teacher.
+     */
     List<ReceptionTimeslotResponse> getSlots(@NotNull UUID teacherID);
+
 }
