9415b2f2dc95b4083948771dc93b068f435b5673
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-01-23 14:34:57 +0100

    test(backend): authentication integration test for authentication

--- src/test/java/it/astromark/authentication/controller/AuthControllerTest.java
+++ src/test/java/it/astromark/authentication/controller/AuthControllerTest.java
@@ -1,10 +1,27 @@
 package it.astromark.authentication.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles(value = "test")
+@ExtendWith(SpringExtension.class)
 class AuthControllerTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:17.2");
+
 
     @Test
     void tc1_01() {
