4d32e68b700bb286f9e61a8b7ece467e3569e59e
MarioFas <m.fasolino26@studenti.unisa.it>
2025-01-25 15:35:05 +0100

    refactor (backend): deleted TeacherClassResponse

--- src/main/java/it/astromark/classmanagement/mapper/ClassManagementMapper.java
+++ src/main/java/it/astromark/classmanagement/mapper/ClassManagementMapper.java
@@ -2,7 +2,6 @@ package it.astromark.classmanagement.mapper;
 
 import it.astromark.classmanagement.dto.SchoolClassResponse;
 import it.astromark.classmanagement.dto.SchoolClassStudentResponse;
-import it.astromark.classmanagement.dto.TeacherClassResponse;
 import it.astromark.classmanagement.entity.SchoolClass;
 import it.astromark.user.student.entity.Student;
 import org.mapstruct.Mapper;
@@ -15,16 +14,12 @@ import java.util.Set;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ClassManagementMapper {
 
+    @Mapping(target = "description", source = "studyPlan.title")
     SchoolClassResponse toSchoolClassResponse(SchoolClass schoolClass);
 
     List<SchoolClassResponse> toSchoolClassResponseList(List<SchoolClass> marks);
 
     List<SchoolClassResponse> toSchoolClassResponseList(Set<SchoolClass> schoolClasses);
-
-    @Mapping(target = "description", source = "studyPlan.title")
-    TeacherClassResponse toTeacherClassResponse(SchoolClass schoolClass);
-
-    List<TeacherClassResponse> toTeacherClassResponseList(List<SchoolClass> schoolClasses);
 
     SchoolClassStudentResponse toSchoolClassStudentResponse(Student student);
 
