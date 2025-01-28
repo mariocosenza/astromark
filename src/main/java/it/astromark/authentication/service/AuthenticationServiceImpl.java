0d3d1eb5a4a2aa94f6d5b64b6bdb98dca600e58f
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-01-28 12:48:02 +0100

    fix(backend): fix regex

--- src/main/java/it/astromark/authentication/service/AuthenticationServiceImpl.java
+++ src/main/java/it/astromark/authentication/service/AuthenticationServiceImpl.java
@@ -49,21 +49,20 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public SchoolUser login(UserLoginRequest user) {
         var schoolUser = findUserInRepositories(user.username(), user.schoolCode(), user.role());
-        if (schoolUser == null) {
-            return null;
-        }
 
-        if (user.username().isBlank() || user.username().length() < 5 || user.username().length() > 256 ||
+
+        if(user.username().isBlank() || user.username().length() < 5 || user.username().length() > 256 ||
+                !user.password().matches("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$") ||
                 !user.schoolCode().matches("^SS\\d{5}$") ||
                 user.role().isBlank()) {
+            if(schoolUser != null && schoolUser.getPendingState() == PendingState.FIRST_LOGIN) {
+                return schoolUser;
+            }
             throw new IllegalArgumentException("Invalid input");
         }
 
-        if(schoolUser.getPendingState() == PendingState.REMOVE) {
-            log.info("User is pending removal");
+        if (schoolUser == null) {
             return null;
-        } else if (schoolUser.getPendingState() == PendingState.NORMAL && !user.password().matches("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")) {
-            throw new IllegalArgumentException("Invalid input");
         }
 
         var hashedPassword = PasswordUtils.hashPassword(user.password());
@@ -71,6 +70,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
             return schoolUser;
 
         log.info("User not found in any repository");
+        // Se nessun utente trovato o password non valida
         return null;
     }
 
